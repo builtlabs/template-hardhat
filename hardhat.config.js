025c9b36c3900054d637f257ec587c8103bc9d78
builtbyfrancis@outlook.com <builtbyfrancis@outlook.com>
2023-12-29 20:00:45 +0000

    update packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -25,7 +25,7 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.22",
+        version: "0.8.23",
         settings: {
           optimizer: {
             enabled: true,
